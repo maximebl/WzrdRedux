75d24d37474f0df04721cf0c56b055a63ab80241
maximebl <maximebl@videotron.ca>
2017-04-08 18:07:11 -0400

    first commit test

---
+++ WzrdRedux/src/components/course/CoursesPage.js
@@ -0,0 +1,11 @@
+﻿import React, { PropTypes } from 'react';
+
+class CoursesPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+            </div>
+        );
+    }
+}
---
+++ WzrdRedux/tools/startMessage.js
@@ -0,0 +1,2 @@
+﻿import colors from 'colors';
+console.log('starting app in dev mode...'.green);
