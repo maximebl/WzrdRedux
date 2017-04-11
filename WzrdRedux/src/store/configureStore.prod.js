3b68448cf6b2a2217ebcb89cdef04e31347adfce
maximebl <maximebl@videotron.ca>
2017-04-10 22:41:33 -0400

    end

---
+++ WzrdRedux/src/store/configureStore.prod.js
@@ -0,0 +1,11 @@
+﻿import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
+
+export default function configureStore(initialState) {
+    return createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(thunk))
+    );
+}
